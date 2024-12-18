735acc121a32b35e8f6f5e992adfe76a1f5936c5
Riana-S <rianasantos808@gmail.com>
2024-12-17 18:15:25 -0800

    First commit

---
+++ app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+   import './App.css';
+
+   function App() {
+     return (
+       <div className="App">
+         <h1>My First React Website</h1>
+         <p>Welcome to my awesome website!</p>
+       </div>
+     );
+   }
+
+   export default App;

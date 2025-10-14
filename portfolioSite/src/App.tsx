7d93502b2a61a5c9066db700d12f885a2a08329e
Riana-S <rianasantos808@gmail.com>
2025-10-14 12:37:22 -1000

    Project set-up

---
+++ portfolioSite/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="mission">
+        <h1>Hi, I'm Riana!!</h1>
+      </section>
+    </>
+  )
+}
+
+export default App

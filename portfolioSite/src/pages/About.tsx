a7ab7fafaf3d0af7beeb359988be2f7c0c8d8099
Riana-S <rianasantos808@gmail.com>
2025-10-30 02:27:43 -1000

    Second about description completed

--- portfolioSite/src/pages/About.tsx
+++ portfolioSite/src/pages/About.tsx
@@ -39,9 +39,9 @@ export default function About() {
                             <p>Last Updated: Oct 2025</p>
                         </div>
                         <hr></hr>
-                        <p><span>Tech Stack:</span> TypeScript, HTML, CSS, React, Vite, Vercel, Git, GitHub, VSCode</p>
+                        <p><span>Tech Stack :</span> TypeScript, HTML, CSS, React, Vite, Vercel, Git, GitHub, VSCode</p>
                         <p>*Some graphics from <a href="https://reactbits.dev/">React Bits</a> & <a href="https://pixelsafari.neocities.org/">Pixel Safari</a>*</p>
-                        <p><span>Why:</span> Showcase my work & express myself!</p>
+                        <p><span>Why :</span> Showcase my work & express myself!</p>
                     </div>
                     <div id="contactAbout">
                         <h3>Contact Info:</h3>
@@ -55,6 +55,34 @@ export default function About() {
                 <section id="realAbout" className={`about-section ${topCardId === 2 ? "visible" : "hidden"}`}>
                     <h2>Actually, who is Riana?</h2>
                     <p>Get to know me beyond corporate vibes!!</p>
+                    <h3>Can’t Leave the House w/o Earbuds:</h3>
+                    <div id="cdsAbout">
+                        <a href="https://open.spotify.com/album/2hPHncbZRuWiCBauEUJxyA?si=eY_51RSlTEu4g3CnSrKhMg"><img className="cd" src="./aboutPics/mito.jpeg"></img></a>
+                        <a href="https://open.spotify.com/album/2VeUxfOnzVKks0UEX681m2?si=ktEWoceMSmqN_iyXzCGy9A"><img className="cd" src="./aboutPics/brb.jpeg"></img></a>
+                        <a href="https://open.spotify.com/album/4NIqCxqP9o8Tp6tGLBqd8O?si=e2MH53fVQZierQyVh3hA8g"><img className="cd" src="./aboutPics/lys2.jpeg"></img></a>
+                        <a href="https://open.spotify.com/album/03guxdOi12XJbnvxvxbpwG?si=8jTVTOtAS1qDc9CDVF-UgQ"><img className="cd" src="./aboutPics/submarine.jpeg"></img></a>
+                        <a href="https://open.spotify.com/album/1hM5xkleU8wr2yOaY2gCMB?si=vnEriCt4TE2kG3XBmpZmNQ"><img className="cd" src="./aboutPics/reaper.jpeg"></img></a>
+                    </div>
+                    <p><span>Fun fact :</span> My Spotify Wrapped is always 100k+ mins every year</p>
+                    <h3>Hobbies:</h3>
+                    <div id="hobbiesAbout">
+                        <div className="hobby">
+                            <img src="/aboutPics/kj.png"></img>
+                            <p>Gaming</p>
+                        </div>
+                        <div className="hobby">
+                            <img src="/aboutPics/buildKit.png"></img>
+                            <p>Build Kits</p>
+                        </div>
+                        <div className="hobby">
+                            <img src="/aboutPics/travel.png"></img>
+                            <p>Travel</p>
+                        </div>
+                        <div className="hobby">
+                            <img src="/aboutPics/art.jpg"></img>
+                            <p>Art</p>
+                        </div>
+                    </div>
                 </section>
                 <section id="moreAbout" className={`about-section ${topCardId === 1 ? "visible" : "hidden"}`}>
                     <h2>Tell me more!</h2>
